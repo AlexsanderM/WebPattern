97328c9d5e339f8b77a45f80cd8a4482570d620f
Malov <malov.alexsander@yandex.ru>
2018-08-12 14:28:36 +0300

    use MVC

---
+++ WebPattern/WebPattern/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebPattern.Models;
+
+namespace WebPattern.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Vasia",
+                Age = 22
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Ne Vasia",
+                Age = 40
+            }
+        };
+
+        public IActionResult Index()
+        {
+            //return Content("test");
+            return View(_employees);
+        }
+    }
+}
---
+++ WebPattern/WebPattern/Models/EmployeeView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPattern.Models
+{
+    public class EmployeeView
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+    }
+}
