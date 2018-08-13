7b7c2a5809935eaed4e1cd036686c22e0853d24f
Malov <malov.alexsander@yandex.ru>
2018-08-13 21:32:37 +0300

    infrastruc 2

---
+++ WebPattern/WebPattern/Infrastructure/IEmployeesData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebPattern.Models;
+
+namespace WebPattern.Infrastructure
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<EmployeeView> GetAll();
+
+        EmployeeView GetByID(int id);
+
+        void AddNew(EmployeeView model);
+
+        void Delete(int id);
+    }
+}
---
+++ WebPattern/WebPattern/Infrastructure/Implementation/InMemoryEmployeesData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebPattern.Models;
+
+namespace WebPattern.Infrastructure.Implementation
+{
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+        private readonly List<EmployeeView> _employees;
+
+        public InMemoryEmployeesData()
+        {
+            _employees = new List<EmployeeView>()
+            {
+                new EmployeeView()
+            {
+                Id = 1,
+                FirstName = "Vasia",
+                Age = 22
+            },
+                new EmployeeView()
+            {
+                Id = 2,
+                FirstName = "Ne Vasia",
+                Age = 40
+            }
+            };
+        }
+
+        public IEnumerable<EmployeeView> GetAll()
+        {
+            return _employees;
+        }
+
+        public EmployeeView GetByID(int id)
+        {
+            return _employees.FirstOrDefault(e => e.Id == id);
+        }
+
+        public void AddNew(EmployeeView model)
+        {
+            model.Id = _employees.Max(e => e.Id) + 1;
+            _employees.Add(model);
+        }
+
+        public void Delete(int id)
+        {
+            var employee = GetByID(id);
+            if (employee != null)
+            {
+                _employees.Remove(employee);
+            }
+        }
+
+    }
+}
